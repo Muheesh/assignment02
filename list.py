8d7b61331bbaca3271a104fa191824d087504893
Muheesh <muheeshas@gmail.com>
2022-03-03 21:27:32 +0530

    assignment02

---
+++ list.py
@@ -0,0 +1,12 @@
+n = int(input("Number of employees"))
+for i in range(n) :
+    a = []
+    name = input("name")
+    age = input("age")
+    designation = input("Designation")
+    salary = input("Salary")
+    a.append(name)
+    a.append(age)
+    a.append(designation)
+    a.append(salary)
+    print(a)
---
+++ palindrome.py
@@ -0,0 +1,6 @@
+num1 = input("Enter the number")
+num3 = num1[::-1]
+if num1 == num3 :
+    print("It is a palindrome")
+else :
+    print("not a palindrome")
